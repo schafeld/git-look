93512837cfd8ec90c0e02c6a10728d906aacd826
Oliver Schafeld <oliver@schafeld.com>
2018-10-20 19:02:22 +0200

    add UserView and snapshot test

---
+++ tests/unit/UserView.spec.js
@@ -0,0 +1,16 @@
+// describe('UserView', () => {
+//   it('works', () => {})
+// })
+
+import { shallowMount } from '@vue/test-utils'
+import UserView from '@/views/UserView'
+
+describe('UserView', () => {
+  it('renders the component', () => {
+    // arrange
+    const wrapper = shallowMount(UserView)
+
+    // assert
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
